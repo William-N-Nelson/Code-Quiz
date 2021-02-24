5d08a8195a3aa0c05233bcaf85f629f142ac7aac
Willie <niles_nelson@me.com>
2021-02-23 20:32:01 -0600

    Questions finished, now for the timer and score-initial saving

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var start = document.getElementById("startBtn")
-var q1 = document.getElementById("")
+var q1 = document.getElementById("question1")
+var q2 = document.getElementById("question2")
 
 function startQuiz() {
     document.getElementById("startBtn").style.display = "none";
@@ -7,5 +8,16 @@ function startQuiz() {
     document.getElementById("question1").style.display = "block";
 }
 
+function question2() {
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "block";
+}
+
+function question3() {
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "block";
+}
+
 startBtn.addEventListener("click", startQuiz);
-    
+q1.addEventListener("click", question2)
+q2.addEventListener("click", question3)
